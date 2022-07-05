8835e944eeeb97da3c2bac0f8ed2834048845813
Walker.L <laura@e-v.co.za>
2022-07-05 20:32:04 +0100

    Equip Concept
    Can right click inventory item to equip.
    Item displays in equipment slot and on player.
    Currently poorly implemented (just a concept test)

---
+++ Assets/Scripts/PlayerClothing.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerClothing : MonoBehaviour
+{
+    [SerializeField] Image headSlot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
