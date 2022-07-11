855870e17032fe78cca668dbeb05e0acd8e20594
Walker.L <laura@e-v.co.za>
2022-07-11 17:58:22 +0100

    Basic Shop Slot
    Displays name, function, price and image

--- Assets/Scripts/ShopSlot.cs
+++ Assets/Scripts/ShopSlot.cs
@@ -1,8 +1,26 @@
+using UnityEngine;
 using System;
+using TMPro;
+using UnityEngine.UI;
 
 public class ShopSlot : MainSlot
 {
+    [SerializeField] TextMeshProUGUI itemName;
+    [SerializeField] TextMeshProUGUI itemFunction;
+    [SerializeField] TextMeshProUGUI itemPrice;
+    //[SerializeField] Image itemImage; 
+ 
     public static event Action<ShopSlot> BuyEvent;
+
+    protected override void Start()
+    {
+        base.Start();
+        itemName.text = item.name;
+        itemFunction.text = ($"Wearable: {item.equipmentType}");
+        itemPrice.text = item.buyPrice.ToString();
+       // itemImage.sprite = item.icon;
+
+    }
 
     public override void InteractWithSlot()
     {
