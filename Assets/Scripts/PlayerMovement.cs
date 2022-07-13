09f4590b9a598ab61efc67ee690ecee9f02a52e1
Walker.L <laura@e-v.co.za>
2022-07-13 16:28:18 +0100

    Fixed player movement and barrier issues
    Player would move a bit more after releasing the key and hitting an obstacle.
    Used normal box colliders on the wall tiles in stead of tile collider. Looked like wall was able to be pushed by player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform movePoint; // empty game object that works like a movement tracker
     [SerializeField] private LayerMask obstacle;
     private float moveSpeed = 5;
+    private bool atMovePoint = true; // whether the player has reached the move point
 
     void Start()
     {
@@ -15,11 +16,44 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Physics2D.OverlapCircle(movePoint.position, 0.2f, obstacle))
+        if (atMovePoint) // if player is at the move point, the move point is able to be moved using the WASD keys
         {
-            print("collide");
+            float xInput = Input.GetAxisRaw("Horizontal");
+            float yInput = Input.GetAxisRaw("Vertical");
+
+            movePoint.Translate(xInput, yInput, 0);
+
+            if (Vector3.Distance(transform.position, movePoint.position) >= 1 && !HitObstacle()) // with == had bug (player stops moving) when moving diagonally 
+            {
+                atMovePoint = false;
+            }
         }
 
+        if (!atMovePoint) // if the move point is a certain distance away from the player
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, movePoint.position) == 0)
+            {
+                atMovePoint = true;
+            }
+        }
+
+        else
+        {
+            movePoint.position = transform.position; // reset move point
+        }
+
+    }
+
+    private bool HitObstacle()
+    {
+        return Physics2D.OverlapCircle(movePoint.position, 0.2f, obstacle);
+    }
+
+
+    private void OldMove()
+    {
         if (!Physics2D.OverlapCircle(movePoint.position, 0.2f, obstacle))
         {
             transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
