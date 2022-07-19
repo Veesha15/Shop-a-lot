3d7d27743544a792985e85ec19e1f07f2c493875
Walker.L <laura@e-v.co.za>
2022-07-19 10:29:29 +0100

    Added sound and music toggles

--- Assets/Scripts/SettingsWindow.cs
+++ Assets/Scripts/SettingsWindow.cs
@@ -6,14 +6,23 @@ using UnityEngine.UI;
 public class SettingsWindow : MonoBehaviour
 {
     [SerializeField] private GameObject settingsWindow;
+
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button quitButton;
+
+    [SerializeField] private Toggle musicToggle;
+    [SerializeField] private AudioSource musicSource;
+
+    [SerializeField] private Toggle soundToggle;
+    [SerializeField] private AudioSource soundSource;
 
 
     private void Awake()
     {
         settingsButton.onClick.AddListener(OpenSettingsWindow);
         quitButton.onClick.AddListener(QuitGame);
+        musicToggle.onValueChanged.AddListener(delegate { ToggleMusic(musicToggle); });
+        soundToggle.onValueChanged.AddListener(delegate { ToggleSound(soundToggle); });
     }
 
     private void OpenSettingsWindow()
@@ -27,4 +36,40 @@ public class SettingsWindow : MonoBehaviour
         Application.Quit();
     }
 
+
+    // TODO: these are the same, find way to reduce code
+    private void ToggleMusic(Toggle _toggle)
+    {
+        AudioManager.Instance.PlayButtonClick();
+
+        if (_toggle.isOn)
+        {
+            musicSource.enabled = true;
+        }
+
+        else
+        {
+            musicSource.enabled = false;
+        }  
+    }
+
+
+    private void ToggleSound(Toggle _toggle)
+    {
+        AudioManager.Instance.PlayButtonClick();
+
+        if (_toggle.isOn)
+        {
+            soundSource.mute = false;
+        }
+
+        else
+        {
+            soundSource.mute = true;
+        }
+    }
+
+
+
 }
+
